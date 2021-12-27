777acf71b9de58d24187ccaa1ac5d4ad571e55cb
janzuurbier <jan.zuurbier@hu.nl>
2021-12-27 22:45:53 +0100

    last commit

--- dag24/dag24.cpp
+++ dag24/dag24.cpp
@@ -6,14 +6,14 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <map>
 #include <set>
-//#include <stack>
 //#include "../matrix/matrix.h"
-#include <algorithm>
+//#include <algorithm>
 
 using namespace std;
 
-int w = 0, x = 0, y = 0, z = 0;
+int64_t w = 0, x = 0, y = 0, z = 0;
 
 vector<string> instructions;
 
@@ -110,7 +110,6 @@ void run() {
 	cout << endl;
 }
 
-
 int main()
 {
 	ifstream input("C:\\Users\\Jan\\Desktop\\input.txt");
@@ -119,26 +118,14 @@ int main()
 		return 1;
 	}
 
-	string line;
-
-
+	string line;	
 	while (getline(input, line)) {
-		instructions.push_back(line);
+		if (line.substr(0, 3) == "inp") {
+			instructions.push_back(line);
+		}
 	}
 
-	while (true) run();
-
-
-
+	run();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
